bbc2eaa076a6de972f6aa4bb717212c2388c0162
moodman58 <mehmoodumar281@gmail.com>
2023-12-31 03:19:02 -0500

    Fixed some shit

--- AiWebserver.py
+++ AiWebserver.py
@@ -5,17 +5,22 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain.memory import ConversationBufferWindowMemory
-
 @app.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
         income = request.form['income']
-        filing_status = request.form['filing_status']
-        year= request.form['year']
+        federal_taxes_withheld = request.form['federal_taxes_withheld']
+        social_security_wage= request.form['social_security_wage']
+        social_security_tax_withheld = request.form['social_security_tax_withheld']
+        medicare_wages_and_tips = request.form['medicare_wages_and_tips']
+        medicare_tax_withheld= request.form['medicare_tax_withheld']
         user_input_dict = {
             "income": income,
-            "filing_status": filing_status,
-            "year": year
+            "federal_taxes_withheld": federal_taxes_withheld,
+            "social_security_wage": social_security_wage,
+            "social_security_tax_withheld": social_security_tax_withheld,
+            "medicare_wages_and_tips":  medicare_wages_and_tips,
+            "medicare_tax_withheld": medicare_tax_withheld
         }
         refundVal = chatbot(user_input_dict)
         return {
