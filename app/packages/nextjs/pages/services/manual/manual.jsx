8a3a20c2f50f2ee69885d9e4eee2734fcffc97ad
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-12-30 22:32:23 -0800

    Update manual form

--- app/packages/nextjs/pages/services/manual/manual.jsx
+++ app/packages/nextjs/pages/services/manual/manual.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function ManualEntryPage() {
   const [box1, setBox1] = useState(0);
@@ -7,57 +7,80 @@ function ManualEntryPage() {
   const [box4, setBox4] = useState(0);
   const [box5, setBox5] = useState(0);
   const [box6, setBox6] = useState(0);
+  const [result, setResult] = useState(0);
+
+  useEffect(() => {
+    console.log("result updated");
+  }, [result]);
 
   const handleSubmit = e => {
     e.preventDefault();
-    const result = taxCalc(box1, box2, box3, box4, box5, box6);
+    setResult = taxCalc(box1, box2, box3, box4, box5, box6);
   };
 
-  const handleInputChange = field = e = {
+  const handleInputChange = field => e => {
     switch (field) {
-        case value:
-
-            break;
-
-        default:
-            break;
+      case "box1":
+        setBox1(e.currentTarget.value);
+        break;
+      case "box2":
+        setBox2;
+        break;
+      case "box3":
+        setBox3;
+        break;
+      case "box4":
+        setBox4;
+        break;
+      case "box5":
+        setBox5;
+        break;
+      case "box6":
+        setBox6;
+        break;
+      default:
+        break;
     }
-  }
+  };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label>
           Box 1 - Wages, tips, other compensation:
-          <input type="number" name="box1" value={formData.box1} onChange={handleInputChange} />
+          <input type="number" name="box1" value={box1} onChange={handleInputChange} />
         </label>
 
         <label>
           Box 2 - Federal income tax withheld:
-          <input type="number" name="box2" value={formData.box2} onChange={handleInputChange} />
+          <input type="number" name="box2" value={box2} onChange={handleInputChange} />
         </label>
 
         <label>
           Box 3 - Social Security wages:
-          <input type="number" name="box3" value={formData.box3} onChange={handleInputChange} />
+          <input type="number" name="box3" value={box3} onChange={handleInputChange} />
         </label>
 
         <label>
           Box 4 - Social Security tax withheld:
-          <input type="number" name="box4" value={formData.box4} onChange={handleInputChange} />
+          <input type="number" name="box4" value={box4} onChange={handleInputChange} />
         </label>
 
         <label>
           Box 5 - Medicare wages and tips:
-          <input type="number" name="box5" value={formData.box5} onChange={handleInputChange} />
+          <input type="number" name="box5" value={box5} onChange={handleInputChange} />
         </label>
 
         <label>
           Box 6 - Medicare tax withheld:
-          <input type="number" name="box6" value={formData.box6} onChange={handleInputChange} />
+          <input type="number" name="box6" value={box6} onChange={handleInputChange} />
         </label>
         <button type="submit">Submit</button>
       </form>
+
+      <div>
+        Your tax {result > 0 ? "refund" : "due"} is {result}.
+      </div>
     </>
   );
 }
