8b64375adcf6d6e78597a7008c5e066dc92dd6d9
Queen Belle V. Dela Cruz <qb.dcruz@gmail.com>
2023-12-31 06:05:29 -0800

    Add background to sun icon

--- app/packages/nextjs/components/SwitchTheme.tsx
+++ app/packages/nextjs/components/SwitchTheme.tsx
@@ -22,7 +22,7 @@ export const SwitchTheme = ({ className }: { className?: string }) => {
       />
       {isMounted() && (
         <label htmlFor="theme-toggle" className={`swap swap-rotate ${!isDarkMode ? "swap-active" : ""}`}>
-          <SunIcon className="swap-on h-5 w-5" />
+          <SunIcon className="swap-on h-5 w-5 bg-secondary rounded-xl" />
           <MoonIcon className="swap-off h-5 w-5" />
         </label>
       )}
